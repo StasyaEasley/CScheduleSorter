4a44fa4da8a3ddb525daa9dac9e47e0d3ca9176a
StasyaEasley <146251595+StasyaEasley@users.noreply.github.com>
2023-10-10 01:31:09 -0700

    Add files via upload

--- dict.py
+++ dict.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 
 html_file = open("jJ4fRq-UA_PCC_XML.html").read()
 soup = BeautifulSoup(html_file, "html.parser")
@@ -19,21 +20,29 @@ for h2 in all_hs:
 
 description_list = []
 career_list = []
-grade_basis_list = []
 course_comp_list = []
+"""grade_basis_list = []"""
+"""enroll_req_list = []"""
 for sublist in h2_list:
     if sublist[0] == 'Description':
         description_list.append(sublist[1])
     elif sublist[0] == 'Career':
         career_list.append(sublist[1])
-    elif sublist[0] == 'Grading basis':
-        grade_basis_list.append(sublist[1])
     elif sublist[0] == 'Course Components':
         course_comp_list.append(sublist[1])
+
+    """elif sublist[0] == 'Grading basis':
+        grade_basis_list.append(sublist[1])"""
+    """elif sublist[0] == 'Enrollment Requirement':
+        enroll_req_list.append(sublist[1])"""
+
 csc_list = []
+spring_fall_list = []
 for sublist in ps_list:
     if 'CSC' in sublist[0]:
         csc_list.append(sublist[0])
+    elif 'Main Campus' in sublist[0]:
+        spring_fall_list.append(sublist[1])
 
 description_file = open("description.tsv", "w")
 for i in range(len(description_list)):
@@ -43,35 +52,52 @@ for i in range(len(description_list)):
     description_file.write("\t")
     description_file.write(career_list[i])
     description_file.write("\t")
+    description_file.write(spring_fall_list[i])
+    description_file.write("\t")
     description_file.write(course_comp_list[i])
     description_file.write("\n")
-    """description_file.write(grade_basis_list[i])
-        description_file.write("\t")"""
+# Issue here is that after the courses become Graduate courses,
+# there are no enrollment reqs. listed, so the index is out of the range
+"""description_file.write(enroll_req_list[i])"""
+# For this one, only CSC 920 does not have a grading basis. I guess I can
+# shorten the range so the index fits, but then CSC 920 is omitted
+"""description_file.write(grade_basis_list[i])"""
 
 description_file.close()
 
+# using this class here because I wanted the key I
+# used to merge my files ot be something nice like CSC 101
 ps3_list = []
 all_ps3 = soup.find_all("p", class_="s3")
 for p in all_ps3:
     this_text = p.text
     ps3_list.append(this_text.split(":"))
+print(ps_list)
 
 new_file = open("course_info.tsv", "w")
 for sublist in ps3_list:
     new_file.write("\t".join(sublist) + "\n")
 new_file.close()
 
-"""res = list(map(lambda x: x if x != [''] else None, ps_list))
-res = [x for x in res if x != None]
-print(str(res))"""
+# combines tsv files
+cour = pd.read_csv('course_info.tsv', sep='\t')
+desc = pd.read_csv('description.tsv', sep='\t')
+
+# shows my key
+"""print(cour.head())
+print(desc.head())"""
+
+merged_data = pd.merge(cour, desc, on='CSC 101')
+merged_data.to_csv('merged_data.tsv', sep='\t', index=False)
+
 
 """
 Need: Course Name & Title (p)
 Description (h2)
 Units (p)
 Career level (h2)
-Grading (h2)
+---Grading (h2)---
 course components (h2)
-enrollment req (h2)
-course typically offered (p)
+enrollment req (h2) ****
+course typically offered (p) 
 """
